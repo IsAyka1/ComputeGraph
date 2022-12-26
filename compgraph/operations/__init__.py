e2cd7ff47cbefbc2176a1b566579a79e9470e33e
IsAyka1 <aigul-isl@yandex.ru>
2022-12-26 16:30:06 +0300

    fixed build.yml, added setup.cfg for mypy/flake8/pytest

--- compgraph/operations/__init__.py
+++ compgraph/operations/__init__.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 
 from .base import Operation, Read, ReadIterFactory, ReadTxtToCSV, TRow, TRowsIterable, TRowsGenerator
 from .external_sort import ExternalSort
@@ -6,4 +5,7 @@ from .join import Join, Joiner, InnerJoiner, OuterJoiner, RightJoiner, LeftJoine
 from .map import Map, Mapper, DummyMapper, Filter, LowerCase, FilterPunctuation, Split, Product, Project, Apply
 from .reduce import Reduce, Reducer, FirstReducer, TopN, Sum, Count, TermFrequency
 
-__all__ = [base.__all__ + external_sort.__all__ + join.__all__ + map.__all__ + reduce.__all__]
+__all__ = ['Operation', 'Read', 'ReadIterFactory', 'ReadTxtToCSV', 'TRow', 'TRowsIterable', 'TRowsGenerator', 'Map',
+           'Mapper', 'DummyMapper', 'Filter', 'LowerCase', 'FilterPunctuation', 'Project', 'Product', 'Split', 'Apply',
+           'Reduce', 'Reducer', 'FirstReducer', 'TopN', 'Sum', 'Count', 'TermFrequency', 'Join', 'Joiner',
+           'InnerJoiner', 'OuterJoiner', 'RightJoiner', 'LeftJoiner', 'ExternalSort']
